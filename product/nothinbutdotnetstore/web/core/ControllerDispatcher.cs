6edc8e47098491caa83b077c07d084f885aa1a79
Matt Richardson <matt@redgumtech.com.au>
2010-08-31 18:42:50 +0100

    fixed dispatching to the front controller

--- product/nothinbutdotnetstore/web/core/ControllerDispatcher.cs
+++ product/nothinbutdotnetstore/web/core/ControllerDispatcher.cs
@@ -5,9 +5,18 @@ namespace nothinbutdotnetstore.web.core
 {
     public class ControllerDispatcher : IHttpHandler
     {
+        private readonly RequestFactory request_factory;
+        private readonly FrontController front_controller;
+
+        public ControllerDispatcher(RequestFactory request_factory, FrontController front_controller)
+        {
+            this.request_factory = request_factory;
+            this.front_controller = front_controller;
+        }
+
         public void ProcessRequest(HttpContext context)
         {
-            throw new NotImplementedException();
+            front_controller.process(request_factory.create_from(context));
         }
 
         public bool IsReusable
