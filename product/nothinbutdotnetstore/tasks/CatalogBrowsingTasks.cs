d31c1ecc8e219492384608449c50f9bd81442a74
Butt3r5 <tjbutterfield@btinternet.com>
2010-09-01 18:35:35 +0100

    Having an issue with Rhino Mocks (potentially)

--- product/nothinbutdotnetstore/tasks/CatalogBrowsingTasks.cs
+++ product/nothinbutdotnetstore/tasks/CatalogBrowsingTasks.cs
@@ -6,11 +6,19 @@ namespace nothinbutdotnetstore.tasks
 {
     public interface CatalogBrowsingTasks
     {
-        //IEnumerable<T> GetAll<T>();
-        //IEnumerable<TResult> GetAllFor<T, TResult>(T filterType);
+        IEnumerable<T> GetAll<T>();
+        IEnumerable<TResult> GetAllFor<T, TResult>(T filterType);
 
         IEnumerable<Department> get_all_departments();
         IEnumerable<Department> get_sub_departments_in(Department departmentname);
         IEnumerable<Product> get_all_products_in(Department dept);
     }
+
+    //public static class IEnumerableExtensions
+    //{
+    //    public static IEnumerable<resultType> by<filterType, resultType>(this IEnumerable<resultType> results)
+    //    {
+    //        return 
+    //    }
+    //}
 }
--- product/nothinbutdotnetstore/tasks/stubs/StubCatalogBrowsingTasks.cs
+++ product/nothinbutdotnetstore/tasks/stubs/StubCatalogBrowsingTasks.cs
@@ -8,6 +8,16 @@ namespace nothinbutdotnetstore.tasks.stubs
 {
     public class StubCatalogBrowsingTasks : CatalogBrowsingTasks
     {
+        public IEnumerable<T> GetAll<T>()
+        {
+            return new List<T>();
+        }
+
+        public IEnumerable<TResult> GetAllFor<T, TResult>(T filterType)
+        {
+            return new List<TResult>();
+        }
+
         public IEnumerable<Department> get_all_departments()
         {
             return Enumerable.Range(1, 100).Select(x => new Department {name = x.ToString("Department 0")});
