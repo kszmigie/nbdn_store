6b40486220d2e2506c432548d0a80526ce27e5ea
KubaS <kszmigie@gmail.com>
2010-09-02 14:51:18 +0100

    creator

--- product/nothinbutdotnetstore/infrastructure/containers/basic/FunctionalDependencyResolver.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/FunctionalDependencyResolver.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class FunctionalDependencyResolver : DependencyResolver
     {
+        private readonly Func<object> _creator;
+
+        public FunctionalDependencyResolver(Func<object> creator)
+        {
+            _creator = creator;
+        }
+
         public object create()
         {
-            throw new NotImplementedException();
+            return _creator();
         }
     }
 }
