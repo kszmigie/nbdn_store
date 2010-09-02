1f20df75d862be966b40e8faf3961392ce57aaa2
KubaS <kszmigie@gmail.com>
2010-09-02 14:06:37 +0100

    aa

--- product/nothinbutdotnetstore/infrastructure/containers/basic/DefaultContainer.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/DefaultContainer.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class DefaultContainer : Container
     {
+        private readonly ResolverRegistry _registry;
+
+        public DefaultContainer(ResolverRegistry registry)
+        {
+            _registry = registry;
+        }
+
         public Dependency an<Dependency>()
         {
-            throw new NotImplementedException();
+            return (Dependency) _registry.get_resolver_to_create(typeof (Dependency)).create();
         }
     }
 }
