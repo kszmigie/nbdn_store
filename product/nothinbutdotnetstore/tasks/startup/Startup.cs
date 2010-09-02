6ce1370582307caa6c29149ed1173e59912e2291
Andreas Böckert <andreas.bockert@gmail.com>
2010-09-02 15:44:45 +0100

    Implemented DefaultResolverRegistry.

--- product/nothinbutdotnetstore/tasks/startup/Startup.cs
+++ product/nothinbutdotnetstore/tasks/startup/Startup.cs
@@ -1,4 +1,7 @@
+using nothinbutdotnetstore.infrastructure;
+using nothinbutdotnetstore.infrastructure.containers;
 using nothinbutdotnetstore.infrastructure.containers.basic;
+using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.tasks.startup
 {
@@ -6,7 +9,16 @@ namespace nothinbutdotnetstore.tasks.startup
     {
         public static void run()
         {
+            ResolverRegistry registry = null; // ToDo: implement
+            var c = new DefaultContainer(registry);
 
+//            registry.register<MapperRegistry>(()) => new Default
+//            registry.register<MappingGateway>(() => new DefaultMappingGateway(IOC.retrieve.an<MapperRegistry>()));
+//            registry.register<RequestFactory>(() => new DefaultRequestFactory(IOC.retrieve.an<MappingGateway>()));
+
+
+
+            IOC.container_resolver = () => c;
         }
     }
 }
