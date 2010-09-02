05c4064d25645ce0b513fe62820bfef86bf917ec
Andreas Böckert <andreas.bockert@gmail.com>
2010-09-02 14:19:33 +0100

    Fixed case of missing dependency

---
+++ reset_incorrect_files.sh
@@ -0,0 +1,6 @@
+git checkout artifacts/specs/hibernate.cfg.xml
+git checkout artifacts/specs/log4net.config.xml
+git checkout artifacts/specs/nothinbutdotnetstore.specs.dll.config
+git checkout product/nothinbutdotnetstore.web.ui/hibernate.cfg.xml
+git checkout product/nothinbutdotnetstore.web.ui/log4net.config.xml
+git checkout product/nothinbutdotnetstore.web.ui/web.config
