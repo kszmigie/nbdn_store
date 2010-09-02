cce664dba06f53236de1435387c5ca5b4e4b4f2e
Paul Houlgate <paul@kclani.com>
2010-09-02 17:51:16 +0100

    Department now knows whether it has properties

--- product/nothinbutdotnetstore/model/Department.cs
+++ product/nothinbutdotnetstore/model/Department.cs
@@ -4,5 +4,6 @@
     {
         public string name { get; set; }
         public Department parent_department { get; set; }
+        public bool has_products { get; set; }
     }
 }
--- product/nothinbutdotnetstore/model/Product.cs
+++ product/nothinbutdotnetstore/model/Product.cs
@@ -2,9 +2,20 @@ namespace nothinbutdotnetstore.model
 {
     public class Product
     {
+        private Department department;
+
         public string name { get; set; }
         public string description { get; set; }
-        public Department parent_department { get; set; }
-       public decimal price { get; set; }
+        
+        public Department parent_department
+        {
+            get { return department; }
+            set
+            {
+                department = value;
+                department.has_products = true;
+            }
+        }
+        public decimal price { get; set; }
     }
 }
